15e8807223b583f607071f35446fa8a7ee157976
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-21 14:09:19 +0530

    Change the Number of sides on your dice

--- app/src/main/java/com/example/addabuttontoanapp_kotlin/main.kt
+++ app/src/main/java/com/example/addabuttontoanapp_kotlin/main.kt
@@ -15,13 +15,16 @@ fun main(){
     val diceRoll = myFirstDice.roll()
     println("Your ${myFirstDice.sides} sided dice rolled ${diceRoll}!")
 
+    myFirstDice.sides = 20
+    println("Your ${myFirstDice.sides} aides dice rolled ${myFirstDice.roll()}")
+
 }
 
 class Dice{
     var sides = 6
 
     fun roll(): Int{
-        val randomNumber = (1..6).random()
+        val randomNumber = (1..sides).random()
         return randomNumber
     }
 }
